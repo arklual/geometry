15dfc7efb0246e48ff920807a8c338075e28399a
arklual <arklual1@gmail.com>
2021-12-05 19:54:55 +0300

    Little refactor polygons

--- polygons/traingles.py
+++ polygons/traingles.py
@@ -1,6 +1,6 @@
-from . import base
 from base import Vector, Point, Angle
 import math
+
 
 class Triangle:
     a = Point(0, 0)
@@ -11,26 +11,26 @@ class Triangle:
         self.a = point_a
         self.b = point_b
         self.c = point_c
-    
+
     def is_isosceles(self):
-        vector1 = Vector(start = self.a, end = self.b)
-        vector2 = Vector(start = self.b, end = self.c)
-        vector3 = Vector(start = self.c, end = self.a)
+        vector1 = Vector(start=self.a, end=self.b)
+        vector2 = Vector(start=self.b, end=self.c)
+        vector3 = Vector(start=self.c, end=self.a)
 
         vector1_value = vector1.get_value()
         vector2_value = vector2.get_value()
         vector3_value = vector3.get_value()
 
-        if vector1_value == vector2_value or vector2 == vector3_value or vector3_value==vector1_value:
+        if vector1_value == vector2_value or vector2 == vector3_value or vector3_value == vector1_value:
             return True
         else:
             return False
 
     def is_rectangular(self):
-        ab_vector = Vector(start = self.a, end = self.b)
-        ac_vector = Vector(start = self.c, end = self.a)
-        bc_vector = Vector(start = self.b, end = self.c)
-        
+        ab_vector = Vector(start=self.a, end=self.b)
+        ac_vector = Vector(start=self.c, end=self.a)
+        bc_vector = Vector(start=self.b, end=self.c)
+
         ang = Angle(ab_vector, ac_vector)
         ang1 = Angle(ab_vector, bc_vector)
         ang2 = Angle(ac_vector, bc_vector)
@@ -45,9 +45,9 @@ class Triangle:
             return False
 
     def is_equilateral(self):
-        vector1 = Vector(start = self.a, end = self.b)
-        vector2 = Vector(start = self.b, end = self.c)
-        vector3 = Vector(start = self.c, end = self.a)
+        vector1 = Vector(start=self.a, end=self.b)
+        vector2 = Vector(start=self.b, end=self.c)
+        vector3 = Vector(start=self.c, end=self.a)
 
         vector1_value = vector1.get_value()
         vector2_value = vector2.get_value()
@@ -59,32 +59,28 @@ class Triangle:
             return False
 
     def perimeter(self):
-        vector1 = Vector(start = self.a, end = self.b)
-        vector2 = Vector(start = self.b, end = self.c)
-        vector3 = Vector(start = self.c, end = self.a)
+        vector1 = Vector(start=self.a, end=self.b)
+        vector2 = Vector(start=self.b, end=self.c)
+        vector3 = Vector(start=self.c, end=self.a)
 
         vector1_value = vector1.get_value()
         vector2_value = vector2.get_value()
         vector3_value = vector3.get_value()
 
-        P = vector3_value + vector2_value + vector1_value
+        p = vector3_value + vector2_value + vector1_value
 
-        return P
+        return p
 
     def area(self):
-        vector1 = Vector(start = self.a, end = self.b)
-        vector2 = Vector(start = self.b, end = self.c)
-        vector3 = Vector(start = self.c, end = self.a)
-        
+        vector1 = Vector(start=self.a, end=self.b)
+        vector2 = Vector(start=self.b, end=self.c)
+        vector3 = Vector(start=self.c, end=self.a)
+
         vector1_value = vector1.get_value()
         vector2_value = vector2.get_value()
         vector3_value = vector3.get_value()
-        P = vector3_value + vector2_value + vector1_value
-        p = P/2
-        area = math.sqrt((p*(p-vector1_value)*(p-vector2_value)*(p-vector3_value)))
-        
+        p = vector3_value + vector2_value + vector1_value
+        p = p / 2
+        area = math.sqrt((p * (p - vector1_value) * (p - vector2_value) * (p - vector3_value)))
+
         return area
-
-
-
-
