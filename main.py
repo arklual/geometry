55578499b0897b90c7352fe8aee94d09f1384422
arklual <arklual1@gmail.com>
2021-12-05 19:56:01 +0300

    Little refactor polygons

--- main.py
+++
@@ -1,5 +0,0 @@
-from polygons.polygons import *
-
-sides = [Vector(value=6, y=4.8), Vector(value=10, y=8), Vector(value=6, y=4.8), Vector(value=10, y=8)]
-p = Quadrangle(sides, angles=[Angle(value=20), Angle(value=20), Angle(value=20), Angle(value=20)])
-print(p.get_area())
