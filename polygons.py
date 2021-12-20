3b1e518f11806bd85bb4929b4b6250bad898ec4d
N-tab <65714812+N-tab@users.noreply.github.com>
2021-12-21 00:37:48 +0300

    Update polygons.py

--- polygons.py
+++ polygons.py
@@ -1,81 +1,49 @@
 from base import *
 
+
 class Polygon:
-	def __init__(self, num_of_vertex, sides: list, angles: list):
-		if len(sides) != num_of_vertex or len(angles) != num_of_vertex: raise Exception("Entered data isn't correct");
-		self.num_of_vertex = int(num_of_vertex)
-		self.data = self.set_fig(sides = sides,  angles = angles)
+	def __init__(self, num_of_vertex: int, sides: list, angles: list):
+		if (len(sides) and len(angles)) != num_of_vertex: raise Exception("Entered data isn't correct");
+		self.num_of_vertex = num_of_vertex
+		self.sides = sides
+		self.angles = angles
 
-	def set_fig(self, sides: list, angles: list):
-		data = {}
-		for i in range (len(sides)):
-			if sides[i] != None: data["s"+str(i+1)] = sides[i];
-			if angles [i] != None: data["a"+str(i+1)] = angles[i];
-		return data
-	
-	def fig(self): return self.data;
-	def keys(self): return list(self.data.keys());
-	
-		
+	def __get_sorted_sides_value(self):
+		value_sides = []
+		for side in self.sides:
+			value_sides.append(side.get_value())
+		return sorted(value_sides)
 
+	
 class Quadrangle(Polygon):
 	def __init__(self, sides, angles):
-		self.data = Polygon(num_of_vertex=4, sides=sides, angles=angles)
-		if not self.__is_quadrangle(sides = sides, angles = angles):
-			raise Exception("It isn't a quadrangle")
+		Polygon(num_of_vertex=4, sides=sides, angles=angles)
+		self.angles = angles
+		self.sides = sides
+		if not self.__is_quadrangle(): raise Exception("It isn't a quadrangle");
 
-	def __is_quadrangle(self, sides, angles):
-		if (len(angles) and len(sides)) == 4: return True;
-		else: return False;
-		
-	def fig(self): return self.data.fig()
-		
+	def __is_quadrangle(self):
+		if len(self.angles) == len(self.sides) == 4: return True;
 
 class Parallelogram(Quadrangle):
-	def __init__(self, sides, angles):
-		self.data = Quadrangle(sides=sides, angles=angles)
-		if not self.__is_parallelogram():
-			raise Exception("It isn't a parallelogram")
-
-	def __is_parallelogram(self):
-		#TODO: Parallelogram test with support of None-values
-		#Do we even need tests? 
-		return True
+	def __init__(self, side1=None, side2=None, angle1=None, angle2=None):
+		Quadrangle(sides=[side1, side2, side1, side2], angles=[angle1, angle2, angle1, angle2])
+		self.angle1,self.angle2, self.side2, self.side1 = angle1, angle2, side2, side1
 	
 	def area (self):
-		a = [i for i in ["a1", "a2", "a3","a4"] if i in self.data.keys()]
-		s = [i for i in ["s1", "s2", "s3","s4"] if i in self.data.keys()]
-		#clac by 2 sides and sin of angle between them
-		if ("s1" in s and "s2" in s) and  "a1" in a: return self.data.fig()["s1"].get_value()*self.data.fig()["s2"].get_value()*self.data.fig()["a1"].get_sin();
-		if ("s1" in s and "s2" in s) and  "a3" in a: return self.data.fig()["s1"].get_value()*self.data.fig()["s2"].get_value()*self.data.fig()["a3"].get_sin();
-		if ("s3" in s and "s4" in s) and  "a1" in a: return self.data.fig()["s3"].get_value()*self.data.fig()["s4"].get_value()*self.data.fig()["a1"].get_sin();
-		if ("s3" in s and "s4" in s) and  "a3" in a: return self.data.fig()["s3"].get_value()*self.data.fig()["s4"].get_value()*self.data.fig()["a3"].get_sin();
-		
-		if ("s2" in s and "s3" in s) and  "a2" in a: return self.data.fig()["s3"].get_value()*self.data.fig()["s2"].get_value()*self.data.fig()["a2"].get_sin();
-		if ("s2" in s and "s3" in s) and  "a4" in a: return self.data.fig()["s3"].get_value()*self.data.fig()["s2"].get_value()*self.data.fig()["a4"].get_sin();
-		if ("s4" in s and "s1" in s) and  "a2" in a: return self.data.fig()["s1"].get_value()*self.data.fig()["s4"].get_value()*self.data.fig()["a2"].get_sin();
-		if ("s4" in s and "s1" in s) and  "a4" in a: return self.data.fig()["s1"].get_value()*self.data.fig()["s4"].get_value()*self.data.fig()["a4"].get_sin();
-		#calc by 2 sides (pls, sbd test this piece of shit)
-		if "s1" in s and "s2" in s: return self.data.fig()["s1"].get_value()*self.data.fig()["s2"].get_value()*(1/((self.data.fig()["s1"].get_x()*self.data.fig()["s2"].get_x() + self.data.fig()["s1"].get_y()*self.data.fig()["s2"].get_y())/(self.data.fig()["s1"].get_value()*self.data.fig()["s2"].get_value())));
-		if "s2" in s and "s3" in s: return self.data.fig()["s3"].get_value()*self.data.fig()["s2"].get_value()*(1/((self.data.fig()["s3"].get_x()*self.data.fig()["s2"].get_x() + self.data.fig()["s3"].get_y()*self.data.fig()["s2"].get_y())/(self.data.fig()["s3"].get_value()*self.data.fig()["s2"].get_value())));
-		if "s4" in s and "s3" in s: return self.data.fig()["s3"].get_value()*self.data.fig()["s4"].get_value()*(1/((self.data.fig()["s3"].get_x()*self.data.fig()["s4"].get_x() + self.data.fig()["s3"].get_y()*self.data.fig()["s4"].get_y())/(self.data.fig()["s3"].get_value()*self.data.fig()["s4"].get_value())));
-		if "s4" in s and "s1" in s: return self.data.fig()["s1"].get_value()*self.data.fig()["s4"].get_value()*(1/((self.data.fig()["s1"].get_x()*self.data.fig()["s4"].get_x() + self.data.fig()["s1"].get_y()*self.data.fig()["s4"].get_y())/(self.data.fig()["s1"].get_value()*self.data.fig()["s4"].get_value())));
-		
-		
+		angle1, angle2, side2, side1 = self.angle1,self.angle2, self.side2, self.side1
+		if (side1 and side2 and angle1) != None: return side1.get_value()*side2.get_value()*angle1.get_sin();
+		elif (side1 and side2 and angle2) != None: return side1.get_value()*side2.get_value()*angle2.get_sin();
+		#place to calc by 2 sides [shit] code
+
 class Rectangle(Parallelogram):
-	def __init__(self, sides):
-		self.data = Parallelogram(sides=sides, angles = [Angle(value=90),Angle(value=90),Angle(value=90),Angle(value=90)])
-	
+	def __init__(self, side1 = None, side2 = None):
+		Parallelogram(side1=side1, side2=side2, angle1=Angle(value=90), angle2=Angle(value=90))
 
 class Rhombus(Parallelogram):
-	def __init__(self, side, angles):
-		self.data = Parallelogram(sides=[side,side,side,side], angles = angles) #finish this..
-		if not self.__is_rhombus():
-			raise Exception("It isn't a rhombus")
-
-	def __is_rhombus(self): return True;
-
+	def __init__(self, side=None, angle2=None, angle1=None):
+		Parallelogram(side1=side, side2=side, angle1=angle1, angle2=angle2)
 
 class Square(Rhombus, Rectangle):
-	def __init__(self, side):
-		self.data = Rhombus.__init__(self=self, side=side, angles = [Angle(value=90),Angle(value=90),Angle(value=90),Angle(value=90)]) 
+	def __init__(self, side=None):
+		Rhombus(side = side, angle1=Angle(value=90), angle2=Angle(value=90))
